bf294ea6c35f225a2e74946945edafb23be7beec
혜준 <167407164+HyejunKoo@users.noreply.github.com>
2025-12-28 15:07:00 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
@@ -1,22 +1,21 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int main(){
-    int N,K;
+int main() {
+    int N, K;
     cin >> N >> K;
-    
-    vector<int> div;
-    
-    for(int i=1; i<=N; i++){
-       if (N%i==0){
-           div.push_back(i);
-       }}
-    
-    if((int)div.size()>=K){
-        cout << div[K-1] <<"\n";
-    }else{
-         cout << 0 << endl;
+
+    int cnt = 0;
+    for (int i = 1; i <= N; i++) {
+        if (N % i == 0) {
+            cnt++;
+            if (cnt == K) {
+                cout << i;
+                return 0;
+            }
+        }
     }
+
+    cout << 0;
     return 0;
-    }
+}
