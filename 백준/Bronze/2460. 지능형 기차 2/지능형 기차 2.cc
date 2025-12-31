0e048a1eca5329cc59e06e4d39196511932d13b8
혜준 <167407164+HyejunKoo@users.noreply.github.com>
2025-12-31 15:09:25 +0900

    [Bronze III] Title: 지능형 기차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
+++ 백준/Bronze/2460. 지능형 기차 2/지능형 기차 2.cc
@@ -1,21 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(){
+    
     int cur = 0;
+    int I, O;
     int mx = 0;
-
-    for (int i = 0; i < 10; i++) {
-        int out, in;
-        cin >> out >> in;
-
-        cur -= out;
-        cur += in;
-
-        if (cur > mx)
+    
+    for(int i=0; i<10; i++){
+        cin >> O >> I;
+        cur+=I;
+        cur-=O;
+        if(mx < cur){
             mx = cur;
+        }
     }
-
     cout << mx;
     return 0;
 }
