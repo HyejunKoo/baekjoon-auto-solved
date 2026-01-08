1bf97082e38bb63c0f7c4baac6808197b28c45d0
혜준 <167407164+HyejunKoo@users.noreply.github.com>
2026-01-08 12:11:01 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -1,29 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(){
     int n;
     cin >> n;
-
-    if (n == 0) {
+    if(n==0){
         cout << 0;
         return 0;
     }
-    if (n == 1) {
-        cout << 1;
-        return 0;
+    int a=0;
+    int b=1;
+    int c=0;
+    
+    for(int i=2; i<=n; i++){
+        c=a+b;
+        a=b;
+        b=c;
     }
-
-    int a = 0; // F(0)
-    int b = 1; // F(1)
-    int c;
-
-    for (int i = 2; i <= n; i++) {
-        c = a + b;
-        a = b;
-        b = c;
-    }
-
     cout << b;
     return 0;
 }
