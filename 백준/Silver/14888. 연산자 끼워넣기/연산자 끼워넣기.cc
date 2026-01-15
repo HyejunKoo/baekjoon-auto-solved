388963eb37c706557a53ebdcbe87e5dd7b677539
혜준 <167407164+HyejunKoo@users.noreply.github.com>
2026-01-15 23:59:12 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.cc
@@ -1,46 +1,41 @@
 #include <iostream>
-#include <vector>
 #include <climits>
 using namespace std;
 
 int N;
-vector<int> A;
+int A[11];
 int op[4]; // +, -, *, /
 int maxVal = INT_MIN;
 int minVal = INT_MAX;
 
 void dfs(int idx, int current) {
-    // idx: 현재 사용할 숫자의 인덱스
+    // 숫자 N개를 다 사용한 경우
     if (idx == N) {
-        maxVal = max(maxVal, current);
-        minVal = min(minVal, current);
+        if (current > maxVal) maxVal = current;
+        if (current < minVal) minVal = current;
         return;
     }
 
+    // 연산자 4개 중 선택
     for (int i = 0; i < 4; i++) {
         if (op[i] > 0) {
-            op[i]--;
-            int next = current;
+            op[i]--;  // 연산자 하나 사용
 
+            int next;
             if (i == 0) next = current + A[idx];
             else if (i == 1) next = current - A[idx];
             else if (i == 2) next = current * A[idx];
-            else if (i == 3) {
-                if (current < 0)
-                    next = - (abs(current) / A[idx]);
-                else
-                    next = current / A[idx];
-            }
+            else if (i == 3) next = current / A[idx]; // C++14 기준 자동 처리
 
             dfs(idx + 1, next);
-            op[i]++; // 되돌리기
+
+            op[i]++;  // 백트래킹
         }
     }
 }
 
 int main() {
     cin >> N;
-    A.resize(N);
 
     for (int i = 0; i < N; i++) {
         cin >> A[i];
@@ -52,6 +47,8 @@ int main() {
 
     dfs(1, A[0]);
 
-    cout << maxVal << '\n' << minVal;
+    cout << maxVal << '\n';
+    cout << minVal << '\n';
+
     return 0;
 }
