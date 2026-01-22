0ce813e7dc58c2eb7d424c80a3006f77077da64f
혜준 <167407164+HyejunKoo@users.noreply.github.com>
2026-01-22 21:11:56 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    long long a = n * n;
+
+    int b = 2;
+
+    // 결과 출력
+    cout << a << '\n';
+    cout << b << '\n';
+
+}
