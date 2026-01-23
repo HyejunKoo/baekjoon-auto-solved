363c6ab96e44fdbb5dd9b0c0563edbe4bbdfab91
혜준 <167407164+HyejunKoo@users.noreply.github.com>
2026-01-23 15:50:20 +0900

    [Bronze II] Title: 분해합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.cc
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -1,45 +1,33 @@
 #include <iostream>
 using namespace std;
 
-// x의 각 자리수 합을 구하는 함수
-int digitSum(int x) {
-    int sum = 0;
-    while (x > 0) {
-        sum += x % 10;
-        x /= 10;
+int digitSum(int x){
+    int sum=0;
+    while (x>0){
+      sum += x % 10;
+      x/=10;
     }
     return sum;
 }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
+int main(){
     int N;
     cin >> N;
-
-    // N의 자릿수 계산
-    int digits = 0;
-    int temp = N;
-    while (temp > 0) {
-        digits++;
-        temp /= 10;
-    }
-
-    // 생성자 후보의 시작점: N - 9*자릿수 (음수면 1부터)
-    int start = N - 9 * digits;
-    if (start < 1) start = 1;
-
-    // start부터 N까지 돌면서 가장 작은 생성자 찾기
-    for (int m = start; m <= N; m++) {
-        int val = m + digitSum(m);
-        if (val == N) {
-            cout << m << "\n";
+    
+    int digits = to_string(N).length();
+    
+    int start = N - 9*digits;
+    if(start<1) start = 1;
+    
+    for(int i=start; i<N; i++){
+        int sum = i+digitSum(i);
+        if (sum == N){
+            cout << i;
             return 0;
         }
     }
-
-    // 생성자가 없으면 0
-    cout << 0 << "\n";
+    cout << 0;
     return 0;
+    
+   
 }
