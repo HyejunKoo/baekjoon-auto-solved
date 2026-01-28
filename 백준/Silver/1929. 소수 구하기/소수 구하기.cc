8bfaa1671484ebe2314decbbce11ea8cf3910c95
혜준 <167407164+HyejunKoo@users.noreply.github.com>
2026-01-28 23:26:24 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void sieve(int M, int N){
+vector<bool> isPrime(N+1, true);
+    isPrime[0]=isPrime[1]=false;
+    
+    for(int i=2; i*i<=N; i++){
+        if(isPrime[i]){
+            for(int j=i*i; j<=N; j+= i){
+                isPrime[j]=false;
+            }
+        }
+    }
+    for(int i=M; i<=N; i++){
+        if(isPrime[i])cout<<i<<'\n';
+    }
+}
+int main(){
+    int M,N;
+    cin >> M >> N;
+    
+sieve(M,N);
+
+    return 0;
+}
