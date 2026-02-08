f3dcac2c348e8a4f36e210b4219c4d83d7a86b51
혜준 <167407164+HyejunKoo@users.noreply.github.com>
2026-02-08 23:02:13 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -1,30 +1,24 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    string n;
-    cin >> n;
+    string s;
+    cin >> s;
 
     int cnt[10] = {0};
 
-    for (char c : n) {
+    for (char c : s) {
         cnt[c - '0']++;
     }
 
     int sixNine = cnt[6] + cnt[9];
-    cnt[6] = (sixNine + 1) / 2;  // 올림 처리
-    cnt[9] = 0;                  // 9는 더 이상 따로 안 씀
+    cnt[6] = (sixNine + 1) / 2;
+    cnt[9] = 0;
 
     int ans = 0;
     for (int i = 0; i < 10; i++) {
         ans = max(ans, cnt[i]);
     }
 
-    cout << ans << "\n";
-    return 0;
+    cout << ans;
 }
