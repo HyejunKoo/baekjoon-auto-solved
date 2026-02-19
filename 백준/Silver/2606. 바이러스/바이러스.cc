33a80c309294be89c9a8a0cc958ac4b24f8177b9
혜준 <167407164+HyejunKoo@users.noreply.github.com>
2026-02-19 23:54:35 +0900

    [Silver III] Title: 바이러스, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.cc
+++ 백준/Silver/2606. 바이러스/바이러스.cc
@@ -1,13 +1,10 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+#include <vector>
 
 vector<int> graph[101];
 bool visited[101];
 int cnt=0;
-
-//DFS함수: 현재 node에서 출발해서 갈 수 있는 모든 컴퓨터 방문
-//-node: 현재 방문할 컴퓨터 번호
 
 void dfs(int node){
     visited[node]=true;
@@ -17,27 +14,28 @@ void dfs(int node){
 
         if(!visited[next]){
             cnt++;
+
             dfs(next);
         }
     }
 }
-
 int main() {
+
     int n,m;
-    
-    cin >> n; 
+
+    cin >> n;
     cin >> m;
 
     for(int i=0; i<m; i++){
-     int a,b;
+        int a,b;
         cin >> a >> b;
 
         graph[a].push_back(b);
         graph[b].push_back(a);
+        
     }
 
     dfs(1);
-
     cout << cnt << '\n';
     return 0;
 }
