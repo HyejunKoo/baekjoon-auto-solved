2e5d01309b9e86f36d9da5cfff29bc04047938f0
혜준 <167407164+HyejunKoo@users.noreply.github.com>
2026-05-10 14:28:56 +0900

    [level 1] Title: 추억 점수, Time: 0.62 ms, Memory: 9.73 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -0,0 +1,19 @@
+def solution(name, yearning, photo):
+    answer = []
+    
+    score = {}
+    
+    for i in range(len(name)):
+        score[name[i]] = yearning[i]
+    
+    for people in photo:
+        total=0
+        
+        for person in people:
+            if person in score:
+                total += score[person]
+                
+        answer.append(total)
+                
+
+    return answer
