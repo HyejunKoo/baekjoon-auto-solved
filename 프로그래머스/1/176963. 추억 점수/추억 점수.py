3097b87a38f7f80e722049f8c09924e5df43deec
혜준 <167407164+HyejunKoo@users.noreply.github.com>
2026-05-24 14:23:08 +0900

    [level 1] Title: 추억 점수, Time: 0.82 ms, Memory: 9.85 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,19 +1,18 @@
 def solution(name, yearning, photo):
-    answer = []
     
+    answer = []
     score = {}
     
     for i in range(len(name)):
-        score[name[i]] = yearning[i]
-    
+        score [name[i]] = yearning[i]
+                  
     for people in photo:
-        total=0
+        total = 0
         
         for person in people:
             if person in score:
                 total += score[person]
-                
+    
         answer.append(total)
-                
-
+    
     return answer
