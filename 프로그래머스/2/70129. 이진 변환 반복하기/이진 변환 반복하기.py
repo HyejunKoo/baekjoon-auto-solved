ab29b4e519c578fa83d3db758c0041079951d61f
혜준 <167407164+HyejunKoo@users.noreply.github.com>
2026-07-04 18:38:47 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 3.56 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,24 +1,17 @@
 def solution(s):
-    
     zero = 0
     count = 0
     
-    while(True):
+    while(s!="1"):
         one = 0
-        for i in s:
-            if i == "0":
+        for word in s:
+            if word == "0":
                 zero += 1
             else:
-                one += 1 # 문자열 안 0, 1 세기 완료
-        final = bin(one)[2:] # 이진변환 한 결과
-        count += 1
-
-        if final != "1":
-            s = final
-        else:
-            break
-    
-    answer = [count,zero]
-    return answer
+                one += 1
+        s = bin(one)[2:]
+        count += 1            
+            
+    return [count, zero]
             
                   
