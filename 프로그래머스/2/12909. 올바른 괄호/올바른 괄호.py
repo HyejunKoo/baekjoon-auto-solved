684ea9f09987d181764820ebe5f078ee3771d843
혜준 <167407164+HyejunKoo@users.noreply.github.com>
2026-07-04 14:35:28 +0900

    [level 2] Title: 올바른 괄호, Time: 5.25 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,18 +1,15 @@
 def solution(s):
+    count = 0
     
-    count = 0    
-    for i in s:
-        if i == "(":
+    for word in s:
+        if word == "(":
             count += 1
         else:
             count -= 1
         if count < 0:
-            return False    
-    
-
-    if s[0] == ")":
-        return False
-    elif count != 0:
+            return False
+    if count != 0:
         return False
     else:
         return True
+        
